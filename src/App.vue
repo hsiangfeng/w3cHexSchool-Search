55ef55b52fecb04c909b021607e4e7088d9ee301
Ray <loveyou520babytw@gmail.com>
2020-06-06 13:27:12 +0800

    add to router title

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,12 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
   name: 'App',
+  watch: {
+    $route(to) {
+      const nowTitle = `${to.meta.title} - W3HexSchool 全馬鐵人搜尋器` || 'W3HexSchool 全馬鐵人搜尋器';
+      document.title = nowTitle;
+    },
+  },
 })
 export default class App extends Vue {
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -13,16 +13,25 @@ const routes = [
         path: '',
         name: 'Index',
         component: () => import('../views/Index.vue'),
+        meta: {
+          title: '全馬鐵人搜尋器首頁',
+        },
       },
       {
         path: '/user',
         name: 'User',
         component: () => import('../components/User.vue'),
+        meta: {
+          title: '全馬鐵人搜尋頁面',
+        },
       },
       {
         path: '/save',
         name: 'Save',
         component: () => import('../components/Save.vue'),
+        meta: {
+          title: '已關注的鐵人',
+        },
       },
     ],
   },
