e7eca29ff6d39a63e2a189bdd7f610bce653eea1
Ray <loveyou520babytw@gmail.com>
2020-08-13 11:51:59 +0800

    deploy

---
+++ precache-manifest.fffee0cec01083c93006451413d5b74c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ddbc8f1ad4e3231fda",
+    "url": "css/app.e266ca55.css"
+  },
+  {
+    "revision": "cea8a151afe6338bd2a4",
+    "url": "css/chunk-3fbae376.76c0d8b7.css"
+  },
+  {
+    "revision": "b5df53e3aa3cd1ffb344",
+    "url": "css/chunk-470af655.a99fd838.css"
+  },
+  {
+    "revision": "2307668159c5ff043e68",
+    "url": "css/chunk-vendors.bf467cf4.css"
+  },
+  {
+    "revision": "b3211768f0369d49165d3d0ef8fa6548",
+    "url": "index.html"
+  },
+  {
+    "revision": "98ddbc8f1ad4e3231fda",
+    "url": "js/app.f0a35bcf.js"
+  },
+  {
+    "revision": "81b1dc4add2755765a8c",
+    "url": "js/chunk-2d0df46a.3973d8cc.js"
+  },
+  {
+    "revision": "cea8a151afe6338bd2a4",
+    "url": "js/chunk-3fbae376.de861593.js"
+  },
+  {
+    "revision": "b5df53e3aa3cd1ffb344",
+    "url": "js/chunk-470af655.3358014c.js"
+  },
+  {
+    "revision": "0beb32abb6d5a8ca99e1",
+    "url": "js/chunk-6d10e9a4.73870cab.js"
+  },
+  {
+    "revision": "2307668159c5ff043e68",
+    "url": "js/chunk-vendors.435de006.js"
+  },
+  {
+    "revision": "5ddd50997867b11eeec3d689cf968967",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
